c6714321c1f951ecc938bb8ba0cfd205bf5e5753
Joseph Guajardo <josephguajardo@ymail.com>
2021-01-14 11:22:34 -0800

    adding functions

--- script.js
+++ script.js
@@ -8,31 +8,54 @@ var num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '=', '+', '[', '{', ']', '}', ':', ';', '"', '<', '>', '.', '?', '~', '|'];
 
 var generateBtn = document.querySelector('#generate');
+var passwordText = document.querySelector('#password');
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
+  
 
   passwordText.value = password;
 }
 
-function generatePassword() 
+function generatePassword(password) 
 {
-  var pwlength = parseInt(prompt('How many characters would you like your password to be? Please choose from 8-128 characters.'));
+  var passwordText = parseInt(prompt('How many characters would you like your password to be? Please choose from 8-128 characters.'));
   {
-    if( length < 8);{
-      prompt("Please select a higher number!");
-    }
-    if(length > 128);{
-      prompt("Please select a lower number!")
+    for (var i=0; i > password; i++){
+    var lowLet = letterLow[Math.floor(Math.random() * letterLow.length)];
 
+    var uplet = letterUp[Math.floor(Math.random() * letterUp.length)];
+
+    var number = num[Math.floor(Math.random() * num.length)];
     }
-    
-   }
-  console.log(typeof pwlength);
-  console.log();
-}
+    {
+      if(num < 8);{
+        prompt("Please select a higher number!");
+      }
+      if(num > 128);{
+        prompt("Please select a lower number!")}
+        
+     }
+
+    var choice = prompt("Please choose the next character in your new password. c for a capital letter, l for a lowercase letter, n for a number or s for a special character.");{
+      if(choice === "c"){
+      document.getElementById("#password").innerHTML += uplet;
+       }
+       else if(choice === "l"){
+      document.getElementById("#password").innerHTML += lowLet;
+       }
+       else if(choice === "n"){
+      document.getElementById("#password").innerHTML += number;
+       }
+       else if(choice === "s"){
+      document.getElementById("#password").innerHTML += spec;
+      }
+      else prompt("That is not a valid choice") 
+  }
+  }
+ }
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
